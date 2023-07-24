f41eb7a15afae97e21e0266d194fe7c647021ba2
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-24 15:04:26 +0200

    Create settet and getters

--- person_class.rb
+++ person_class.rb
@@ -1,7 +1,16 @@
 class Person
+  # Constructor
   def initialize(name = "Unknown", age , parent_permission = true)
     @id = id
     @name = name
     @age = age
   end
+
+  # Getter
+
+  attr_reader :id
+
+  attr_accessor :name :age
+  
+
 end
