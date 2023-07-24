d033d05b126679859690e290c0450ae68163c189
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-24 17:15:39 +0200

    Add license

--- student_class.rb
+++ student_class.rb
@@ -1,6 +1,8 @@
 require_relative 'person_class'
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(classroom)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
