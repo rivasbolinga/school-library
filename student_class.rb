3cc0cd52b67c862ae9ec33c86dd4c37c5d2040da
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-24 15:59:00 +0200

    Add super with the arguments of parent class Person

--- student_class.rb
+++ student_class.rb
@@ -2,9 +2,11 @@ require_relative 'person_class'
 
 class Student < Person
 
-  def initialize(name = "Unknown", age = nil, parent_permission = true)
+  # Constructor extends parent's constructor by adding @classroom and a parameter for it.
+
+  def initialize(name = "Unknown", age = nil, parent_permission = true, classroom)
+    super(name, age, parent_permission)
     @classroom = classroom
   end
-
   
 end
