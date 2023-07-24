22266e5b7eec4624bb53ec0c386c377b5db16768
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-24 15:22:53 +0200

    Finish person class

--- person_class.rb
+++ person_class.rb
@@ -1,17 +1,19 @@
 class Person
-  # Constructor
+  # Constructor with instance variables
   def initialize(name = "Unknown", age , parent_permission = true)
-    @id = id
+    @id = generate_id
     @name = name
     @age = age
   end
 
-  # Getter
+  # Getters and setteres
 
   attr_reader :id
 
   attr_accessor :name :age
-  
+
+  # Private method of_age? that returns true if @age is greater or equal to 18 and false otherwise.
+
   private
 
   def of_age?
@@ -20,5 +22,16 @@ class Person
     else 
       return false
   end
+
+  # Public method can_use_services? that returns true if person is of age or if they have permission from parents.
+
+  def can_use_services?
+    if of_age?  || @parent_permission
+    return true
+  end
+
+  def generate_id
+    rand(1_000..9_999)
+  end  
 
 end
