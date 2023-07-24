ec3c8c0b2f611fa646cba580386c5875e6b39f8e
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-24 16:11:44 +0200

    Constructor extends parent's constructor by adding @spezialitation and a parameter for it.

--- teacher_class.rb
+++ teacher_class.rb
@@ -1,4 +1,12 @@
 require_relative 'student_class'
 
 class Teacher < Person
+  attr_reader :specialization
+
+  # Constructor extends parent's constructor by adding @spezialitation and a parameter for it.
+
+  def initialize(name = "Unknown", age = nil, parent_permission = true, specialization)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
 end
