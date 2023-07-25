ab5df8b498dd669a23632bb1529b519d389de855
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-25 15:20:24 +0200

    Implement a method  that makes sure that the output of  has a maximum of 10 characters. If it's longer it should trim the word.

--- decorators/trimmer_decorator.rb
+++ decorators/trimmer_decorator.rb
@@ -1,5 +1,7 @@
 require_relative './decorator'
 
 class TrimmerDecorator < Decorator
-
+  def correct_name
+    @nameable.correct_name.lenght > 10 ? @nameable.correct_name.strip! : @nameable.correct_name
+  end
 end
