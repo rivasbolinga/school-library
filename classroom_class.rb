794fc8d0ba1e358bc10dffce93266386f2791b5d
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-26 12:44:41 +0200

    Create has-many relationshit between a classroom and stundents

--- classroom_class.rb
+++ classroom_class.rb
@@ -1,7 +1,13 @@
 class Classroom
   attr_accessor :label
+  attr_reader :students
 
   def initialized(label)
     @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student)
   end
 end
