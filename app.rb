542af1a9eecb5cc281445c99742fcbb5e657ec15
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-27 14:35:52 +0200

    Create list_all_books and list_all_people methods in App class

--- app.rb
+++ app.rb
@@ -1,13 +1,27 @@
+
+
 class App
-puts "Please choose an option by enterin a number:"
-puts "1 - List all books"
-puts "2 - List all people"
-puts "3 - Create a person"
-puts "4 - Create a book"
-puts "5 - Create a rental"
-puts "6 - List all rentals for a given person id"
-puts "7 -  Exit"
-selection = gets.chomp
-puts "You have chosen #{selection}!"
+  def initialize()
+    @people = []
+    @books = []
+    @rentals = []
+  end
+
+
+def list_all_books
+  @books.each { | book| puts " #{book}" }
+
+end
+
+def list_all_people
+  @people.each { | book| puts " #{book}" }
+end
+
+def create_a_person
+  puts "Do you want to create a student(1) or a teacher(2)? [input the number]:"
+  input = gets.chomp
+  
+end
+
 end
 
