86e985c44bbbae81e27a7ccc6a8749e5190ebc80
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-27 15:27:30 +0200

    Finish create_teacher method

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative 'classes/student_class'
+require_relative 'classes/teacher_class'
 
 class App
   def initialize()
@@ -20,9 +21,9 @@ end
 def create_a_person
   puts "Do you want to create a student(1) or a teacher(2)? [input the number]:"
   input = gets.chomp
-  if input = 1
+  if input == '1'
     create_student()
-  elsif input = 2
+  elsif input == '2'
     create_teacher()
   else
     puts "Please select one of the options"
@@ -41,11 +42,21 @@ def create_student
   puts "What is the classroom of the student?:"
   classroom = gets.chomp
   new_student = Student.new(age,name,parent_permission, classroom)
-  puts "#{name} Student created succesfully!"
+  puts "#{name} as student, has been created succesfully!"
   @people.push(new_student)
 end
 
 def create_teacher
+  puts "You have selected create a teacher, please insert the following"
+  puts "Age:"
+  age = gets.chomp
+  puts "Name:"
+  name = gets.chomp
+  puts "What does he/she teach?"
+  specialization = gets.chomp
+  new_teacher = Teacher.new(age,name, specialization)
+  puts "#{name} as teacher, has been created succesfully!"
+  @people.push(new_teacher)
 end
 
 end
--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative 'app'
 
 def main
   app = App.new()
-  app.list_all_people()
+  app.create_a_person()
 end
 
 main()
