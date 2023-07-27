993c2c888da6f71409c69ac09172aa481ddb8afd
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-27 12:07:24 +0200

    Create a class App that console the different selections

---
+++ app.rb
@@ -0,0 +1,13 @@
+class App
+puts "Please choose an option by enterin a number:"
+puts "1 - List all books"
+puts "2 - List all people"
+puts "3 - Create a person"
+puts "4 - Create a book"
+puts "5 - Create a rental"
+puts "6 - List all rentals for a given person id"
+puts "7 -  Exit"
+selection = gets.chomp
+puts "You have chosen #{selection}!"
+end
+
--- main.rb
+++ main.rb
@@ -1,24 +1,7 @@
-require_relative 'classes/book_class'
-require_relative 'classes/person_class'
-require_relative 'classes/rental_class'
+require_relative 'app'
 
-# Create books
-book1 = Book.new(1, 'Book 1')
-book2 = Book.new(2, 'Book 2')
-book3 = Book.new(3, 'Book 3')
+def main
+  app = App.new()
+end
 
-# Create persons
-person1 = Person.new('John')
-person2 = Person.new('Jane')
-person3 = Person.new('Ana')
-
-# Create rentals and establish relationships
-Rental.new('2017-12-22', book1, person1)
-Rental.new('2017-12-31', book1, person2)
-Rental.new('2018-12-31', book2, person1)
-Rental.new('2022-10-22', book3, person3)
-
-puts book2.rentals.map(&:date)
-puts book1.rentals.map(&:date)
-puts person1.rentals.count
-puts person3.rentals.count
+main()
