c047ca30bb52ee131fdcbd9a547c1901d60087fb
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-28 15:18:09 +0200

    Introduce bug

--- classes/book_class.rb
+++ classes/book_class.rb
@@ -5,7 +5,6 @@ class Book
 
   def initialize(title, author)
     @title = title
-    @author = author
     @rentals = []
   end
 
--- classes/rental_class.rb
+++ classes/rental_class.rb
@@ -6,8 +6,6 @@ class Rental
 
   def initialize(date, person, book)
     @date = date
-    @person = person
-    person.rentals << self
     @book = book
     book.rentals << self
   end
