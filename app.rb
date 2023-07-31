78b5aac23d9418b9716b5c60fbaf615a28df79fb
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-07-31 16:38:51 +0200

    Fix linters

--- app.rb
+++ app.rb
@@ -6,16 +6,22 @@ require_relative 'classes/list_elements_class'
 require_relative 'classes/create_elements_class'
 
 class App
-  attr_accessor :books, :people, :rentals  def initialize
+  attr_accessor :books, :people, :rentals
+
+  def initialize
     @people = []
     @books = []
     @rentals = []
     @list_elements = ListElements.new(@books, @people, @rentals)
     @creale_elements = CreateElements.new(@books, @people, @rentals)
-  end  def exit_app
+  end
+
+  def exit_app
     puts 'Exiting the application. Goodbye!'
     exit
-  end  def apply_option(option)
+  end
+
+  def apply_option(option)
     actions = {
       '1' => @list_elements.method(:list_all_books),
       '2' => @list_elements.method(:list_all_people),
@@ -24,13 +30,16 @@ class App
       '5' => @creale_elements.method(:create_rental),
       '6' => @list_elements.method(:list_rentals_by_id),
       '7' => method(:exit_app)
-    }    action = actions[option]
+    }
+    action = actions[option]
     if action
       action.call
     else
       puts 'You have to select one of the options'
     end
-  end  def run
+  end
+
+  def run
     ui = UI.new
     ui.welcome
     loop do
