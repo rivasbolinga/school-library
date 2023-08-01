a0148bdfc9b78e8dc61b3a5870f1cc1d6754f087
baqar-abbas <baqar007@hotmail.com>
2023-08-01 20:39:28 +0500

    Git Commit by: Baqar Abbas - Added three menthods load_books, load_people and load_rentals for loading app data

--- app.rb
+++ app.rb
@@ -39,6 +39,53 @@ class App
     end
   end
 
+  def load_data
+    load_books
+    load_people
+    load_rentals
+  end
+
+  def load_books
+    if File.exist?('data/books.json')
+      data = JSON.parse(File.read('data/books.json'))
+      @books = data.map { |book_data| Book.new(book_data['title'], book_data['author']) }
+    else
+      puts 'No books data file found. Starting with an empty book list.'
+      @books = []
+    end
+  end
+
+  def load_people
+    if File.exist?('data/people.json')
+      data = JSON.parse(File.read('data/people.json'))
+      @people = data.map do |person_data|
+        if person_data['type'] == 'Student'
+          Student.new(person_data['age'], person_data['name'], true, person_data['classroom'])
+        else
+          Teacher.new(person_data['age'], person_data['name'], person_data['specialization'])
+        end
+      end
+    else
+      puts 'No people data file found. Starting with an empty people list.'
+      @people = []
+    end
+  end
+
+  def load_rentals
+    if File.exist?('data/rentals.json')
+      data = JSON.parse(File.read('data/rentals.json'))
+      @rentals = data.map do |rental_data|
+        book = @books.find { |book| book.title == rental_data['book']['title'] && book.author == rental_data['book']['author'] }
+        person = @people.find { |person| person.name == rental_data['person']['name'] }
+
+        Rental.new(rental_data['date'], person, book) if book && person
+      end.compact
+    else
+      puts 'No rentals data file found. Starting with an empty rentals list.'
+      @rentals = []
+    end
+  end
+
   def save_data
     books_data = @books.map { |book| @convert_data.convert_book(book) }
     people_data = @people.map { |person| @convert_data.convert_person(person) }
