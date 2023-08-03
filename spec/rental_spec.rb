6bdb33f318ddbc95bb6dc2c6454a041a5e0d99c4
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-03 12:59:09 +0200

    Create test for rentals

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,43 +1,47 @@
 require 'rspec'
-require_relative '../classes/book_class'
 require_relative '../classes/rental_class'
 require_relative '../classes/person_class'
+require_relative '../classes/book_class'
 
-describe Rentals do
+describe Rental do
   before(:each) do
-    @rental = Rental.new('10-10-2000' ,)
+    @person = Person.new(333, 'Andrea', 28)
+    @book = Book.new('New Title', 'New Book')
+    @rental = Rental.new('02-02-2000', @person, @book, 1)
   end
 
-  context 'Create a new book' do
-    it 'add title and author' do
-      expect(@book.title).to eq('New Title')
-      expect(@book.author).to eq('New Book')
+  context 'Create a new rental' do
+    it 'adds a new rental' do
+      expect(@rental.date).to eq('02-02-2000')
+      expect(@rental.person).to eq(@person)
+      expect(@rental.book).to eq(@book)
+      expect(@rental.id).to eq(1)
+    end
+
+    it 'generates an ID randomly if ID is not provided' do
+      rental = Rental.new('02-02-2000', @person, @book, nil)
+
+      expect(rental.id).to be_a(Integer)
     end
   end
 
-  context 'Add rentals to the book' do
-    it 'has an empty rentals array initially' do
-      expect(@book.rentals).to be_empty
+  context 'Generate ID' do
+    it 'generates a random ID between 1 and 1000' do
+      rental = Rental.new('02-02-2000', @person, @book, nil)
+
+      expect(rental.send(:generate_id)).to be >= 1
+      expect(rental.send(:generate_id)).to be <= 1000
     end
-
-    it 'can add a rental' do
-      alice = Person.new('Alice')
-      rental = Rental.new('2023-08-03', alice, @book, 1)
-      @book.add_rental(rental)
-
-      expect(@book.rentals).to include(rental)
-    end
-
   end
 
-  context 'Convert to a hash' do
-    it 'converts to a hash' do
-      expected_hash = {
-        title: 'New Title',
-        author: 'New Book'
-      }
+  context 'Check rental in other classess' do
+    it 'adds the rental to the person rentals' do
+      expect(@person.rentals).to include(@rental)
+    end
 
-      expect(@book.to_hash).to eq(expected_hash)
+    it 'adds the rental to the book rentals' do
+      expect(@book.rentals).to include(@rental)
     end
   end
 end
+
